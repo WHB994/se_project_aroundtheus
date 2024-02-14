c2adeb6cb36939f80de6217438d195961aa3a12d
Harrison Boyce <whboyce94@yahoo.com>
2024-02-14 08:15:18 -0600

    revisions for part 3 and 4

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,80 @@
 const initialCards = [
     {
-        name: "Yosemite Valley",
-        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg"
+      name: "Yosemite Valley",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
     },
-
-     {
-        name: "Lake Louise",
-        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg"
-    },
-
     {
-        name: "Bald Mountains",
-        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg"
+      name: "Lake Louise",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
     },
-
     {
-        name: "Latemar",
-        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg"
+      name: "Bald Mountains",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
     },
-
     {
-        name: "Vanoise National Park",
-        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg"
+      name: "Latemar",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
     },
-
     {
-        name: "Lago di Braies",
-        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg"
-    }
-
-
-]
-
-console.log(initialCards);
-
+      name: "Vanoise National Park",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+    },
+    {
+      name: "Lago di Braies",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg ",
+    },
+  ];
+  
+  /* Elements */
+  const profileEditButton = document.querySelector("#profile-edit-button");
+  const profileEditModal = document.querySelector("#profile-edit-modal");
+  const profileEditCloseButton = profileEditModal.querySelector(".modal__close");
+  const profileTitle = document.querySelector(".profile__title");
+  const profileDescription = document.querySelector(".profile__description");
+  const profileTitleInput = document.querySelector("#profile-title-input");
+  const profileDescriptionInput = document.querySelector(
+    "#profile-description-input"
+  );
+  const profileEditForm = profileEditModal.querySelector(".modal__form");
+  const profileEditSubmit = profileEditModal.querySelector("#edit-save-button");
+  const cardListEl = document.querySelector(".cards__list");
+  const cardTemplate =
+    document.querySelector("#card-template").content.firstElementChild;
+  
+  /* Functions */
+  function closePopUp() {
+    profileEditModal.classList.remove("modal_opened");
+  }
+  
+  function handleProfileEditSubmit(e) {
+    e.preventDefault();
+    profileTitle.textContent = profileTitleInput.value.trim();
+    profileDescription.textContent = profileDescriptionInput.value.trim();
+    closePopUp();
+  }
+  
+  function getCardElement(data) {
+    const cardElement = cardTemplate.cloneNode(true);
+    const cardImageEl = cardElement.querySelector(".card__image");
+    const cardTitleEl = cardElement.querySelector(".card__title");
+    cardTitleEl.textContent = data.name;
+    cardImageEl.src = data.link;
+    cardImageEl.alt = data.link;
+    return cardElement;
+  }
+  
+  /* Event Listener */
+  profileEditButton.addEventListener("click", () => {
+    profileTitleInput.value = profileTitle.textContent;
+    profileDescriptionInput.value = profileDescription.textContent.trim();
+    profileEditModal.classList.add("modal_opened");
+  });
+  
+  profileEditCloseButton.addEventListener("click", closePopUp);
+  
+  profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+  
+  initialCards.forEach((cardData) => {
+    const cardElement = getCardElement(cardData);
+    cardListEl.prepend(cardElement);
+  });
